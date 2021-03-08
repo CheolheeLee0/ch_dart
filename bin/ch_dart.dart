82fff85864f6eb3b395f24c69094839c08ce5df2
CheolheeLee <wks0968@gmail.com>
2021-03-08 23:49:37 +0900

    Initial commit

---
+++ bin/ch_dart.dart
@@ -0,0 +1,5 @@
+import 'package:ch_dart/ch_dart.dart' as ch_dart;
+
+void main(List<String> arguments) {
+  print('Hello world: ${ch_dart.calculate()}!');
+}
---
+++ test/ch_dart_test.dart
@@ -0,0 +1,8 @@
+import 'package:ch_dart/ch_dart.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
