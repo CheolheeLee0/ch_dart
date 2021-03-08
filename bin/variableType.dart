a73f34186e69e91df48a6a1246e62023e93047c3
CheolheeLee <wks0968@gmail.com>
2021-03-08 23:56:23 +0900

    Add variableType.dart

---
+++ bin/variableType.dart
@@ -0,0 +1,29 @@
+// import 'package:ch_dart/ch_dart.dart' as ch_dart;
+
+// String
+var name = 'name';
+String namea = 'namea';
+
+// int
+var num = 1234;
+int numa = 1234;
+
+// double
+var dou = 3.14;
+double doua = 3.14;
+
+// List<String>
+var list = ['apple', 'google', 'facebook', 'amazon'];
+List<String> lista = ['apple', 'google', 'facebook', 'amazon'];
+
+// Map<String, Object>, (key:value)
+var map = {'one': 'second', 'two': 'hongjinho', 'three': 'tree'};
+Map<String, Object> mapa = {
+  'one': 'second',
+  'two': 'hongjinho',
+  'three': 'tree'
+};
+
+void main(List<String> arguments) {
+  print('hello');
+}
