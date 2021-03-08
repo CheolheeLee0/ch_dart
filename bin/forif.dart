ca2f5f58284edc817b68a17d5b7645d0d034323d
CheolheeLee <wks0968@gmail.com>
2021-03-09 00:21:31 +0900

    Add forif.dart

---
+++ bin/forif.dart
@@ -0,0 +1,24 @@
+bool isEven(int x) {
+  if (x & 2 == 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+// Iterable
+List<int> getEvenNumbers(Iterable<int> numbers) {
+  // List
+  var evenNumbers = <int>[];
+  // loop iterable<int>
+  for (var i in numbers) {
+    if (isEven(i)) evenNumbers.add(i);
+  }
+  return evenNumbers;
+}
+
+main() {
+  //
+  var numbers = List.generate(10, (i) => 1);
+  print(getEvenNumbers(numbers));
+}
